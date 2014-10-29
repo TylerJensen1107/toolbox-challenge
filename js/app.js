44d4b209e2784e4ad840e354984e196981b0646d
tyler <tylerjensen1107@gmail.com>
2014-10-29 11:59:41 -0700

    class 2

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
 	for (var idx = 1; idx <= 32; idx++) {
 		tiles.push({
 			tileNum: idx,
-			src: 'img/tile' + idx + '.jpg'
+			src: 'img/tile' + idx + '.jpg',
+			canFlip: true
 		});
 	}
 
@@ -38,28 +39,70 @@ $(document).ready(function() {
 	gameBoard.append(row);
 
 	var images = $('#game-board img');
+
+	var matches = 0;
+	var matchesLeft = 8;
+	var missed = 0;
+
+	var tileOne;
+	var tileTwo;
+	var imgLast;
+
 	images.click(function(){
 		var img = $(this);
 		var tile = img.data('tile');
-		img.fadeOut(100, function() {
-			if(tile.flipped) {
-				img.attr('src', 'img/tile-back.png');
-			} else {
-				img.attr({
-					src: tile.src
-				});
-			}
-			tile.flipped = !tile.flipped;
-			img.fadeIn(100);
+		if(tile.canFlip) {
+			img.fadeOut(100, function() {
+				tileTwo = tileOne;
+				tileOne = tile;
+				missed++;
+				// if(tile.flipped) {
+				// 	img.attr('src', 'img/tile-back.png');
+				// } else {
+					img.attr({
+						src: tile.src
+					});
+				//}
+				//tile.flipped = !tile.flipped;
+				tile.canFlip = false;
+				img.fadeIn(100);
 
-		});
+				console.log(tileOne);
+				console.log(tileTwo);
+
+				//If we have chosen two tiles
+				if(tileOne != null && tileTwo != null) {
+					if(tileOne.tileNum == tileTwo.tileNum) {
+						matches++;
+						matchesLeft--;
+					} else {
+						//Because the code after set timeout runs before set timeout, all the variables are reset. To
+						//Solve this I created copies
+						var tileOneCopy = tileOne;
+						var tileTwoCopy = tileTwo;
+						var imgCopy = img;
+						var imgLastCopy = imgLast;
+
+						window.setTimeout(function() {
+							tileOneCopy.canFlip = true;	
+							tileTwoCopy.canFlip = true;
+							imgCopy.attr('src', 'img/tile-back.png');
+							imgLastCopy.attr('src', 'img/tile-back.png');
+						}, 1000);
+					}
+					tileOne = null;
+					tileTwo = null;
+				}
+				imgLast = img;
+			});
+		}
 	});
 
 	var startTime = _.now();
 	var timer = window.setInterval(function() {
 		var elapsedSeconds = Math.floor((_.now() - startTime) / 1000);
 		$('#elapsed-seconds').text(elapsedSeconds);
-		if(elapsedSeconds > 10) {
+		if(matches == 8) {
 			window.clearInterval(timer);
 		}
 	}, 1000);
